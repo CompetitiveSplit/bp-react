1c0150a17b39aa85058b92f380d1bd8ce8c714f7
CompetitiveSplit <narvaez0526@gmail.com>
2024-09-19 09:20:41 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import milogo from "./assets/img/logoReact.png"
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      <img src={milogo} alt="Logo de React"/>
+      <h1>¡Hola mundo!</h1>
+      <p>Este es mi primer proyecto en React</p>
+     </div>
+    </>
+  )
+}
+
+export default App
